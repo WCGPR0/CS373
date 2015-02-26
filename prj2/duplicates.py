1f923b3639ea041fe8c70721bb07f96462c34d45
WCGPR0 <victor_moove@hotmail.com>
2015-02-26 05:37:11 -0500

    Added duplicate states for states that has reject and accept logics.

---
+++ prj2/duplicates.py
@@ -0,0 +1,4 @@
+arr1 = ["LRLRL", "RRRRL", "LLRRL", "RLLRL", "LLLLL", "RRLLL", "RRLLL", "RLRLL", "LRRRR", "LRRRR", "RLLLR", "LRLRL", "RRRRL", "LLRRL", "LLRRL", "RRLRR", "RRLRR", "LLLRR", "RLLRL", ]
+arr2 = ["RLRLL", "LLLRR", "RRRRL", "LRLLR", "RLRRR", "RLRRR", "LRRLL", "LRRLL", "RRRLR", "RRRLR", "LLRLR", "LLRLR", "RRLLL", "LLLLL", "RLLLR", "LLLRR", "RRRRL", "RLRRR", "LRRLL", "LRLLR", "RLRRR", "LRRLL" ]
+
+print(set(arr1) & set(arr2))
